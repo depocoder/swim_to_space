51fdedbaacd331fdee27efc77b3ca1e057c5c60c
herypank <60302361+herypank@users.noreply.github.com>
2020-09-03 14:19:14 +0300

    Добавил рамку в вывод.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import curses
 def draw(canvas):
     row, column = (5, 20)
     canvas.addstr(row, column, 'Hello, World!')
+    canvas.border()
     canvas.refresh()
-    time.sleep(1)
+    time.sleep(10)
   
 if __name__ == '__main__':
     curses.update_lines_cols()
