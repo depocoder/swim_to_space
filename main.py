7f5976c1fd39c6bac1fff6cf782f2666695adf7b
herypank <60302361+herypank@users.noreply.github.com>
2020-09-04 15:38:18 +0300

    Добавил вывод мерцающией звезды.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import curses
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.border()
-    canvas.refresh()
-    time.sleep(10)
+    for brightness, times in ((curses.A_DIM, 2), (curses.A_NORMAL, 0.3), (curses.A_BOLD, 0.3), (curses.A_NORMAL, 0.5)):
+        canvas.addstr(row, column, '*', brightness)
+        canvas.border()
+        canvas.refresh()
+        time.sleep(times)
   
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
+    curses.curs_set(False)
