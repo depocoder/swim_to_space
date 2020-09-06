3e53d09c78e44c6862d4fb773ed2e7c563a679be
herypank <60302361+herypank@users.noreply.github.com>
2020-09-06 22:59:36 +0300

    Добавил функцию blink.

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import time
 import curses
+import asyncio
 
 
-def draw(canvas):
+async def blink(canvas, symbol='*'):
+    row, column = (5, 20)
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(2)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0.3)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0.3)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0.5)
+
+
+"""def draw(canvas):
     row, column = (5, 20)
     for brightness, times in ((curses.A_DIM, 2), (curses.A_NORMAL, 0.3), (curses.A_BOLD, 0.3), (curses.A_NORMAL, 0.5)):
-        canvas.addstr(row, column, '*', brightness)
+        canvas.addstr(row, column, '*****', brightness)
         canvas.border()
         canvas.refresh()
-        time.sleep(times)
+        time.sleep(times)"""
   
+
 if __name__ == '__main__':
-    curses.update_lines_cols()
-    curses.wrapper(draw)
-    curses.curs_set(False)
+    while True:
+        curses.update_lines_cols()
+        curses.wrapper(blink)
+        curses.curs_set(False)
