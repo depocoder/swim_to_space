33b6e5cf9f47d0c5aac262053cda215d63cf1065
herypank <60302361+herypank@users.noreply.github.com>
2020-09-07 15:01:18 +0300

    Исправил функцию blink, теперь она корректно работает.

--- main.py
+++ main.py
@@ -3,33 +3,32 @@ import curses
 import asyncio
 
 
-async def blink(canvas, symbol='*'):
-    row, column = (5, 20)
+async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(2)
+        await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(0)
 
 
-"""def draw(canvas):
-    row, column = (5, 20)
-    for brightness, times in ((curses.A_DIM, 2), (curses.A_NORMAL, 0.3), (curses.A_BOLD, 0.3), (curses.A_NORMAL, 0.5)):
-        canvas.addstr(row, column, '*****', brightness)
+def draw(canvas):
+    courutine = blink(canvas, row=5, column=20)
+    for i in range(5):
+        courutine.send(None)
         canvas.border()
         canvas.refresh()
-        time.sleep(times)"""
+        time.sleep(1)
   
 
 if __name__ == '__main__':
     while True:
         curses.update_lines_cols()
-        curses.wrapper(blink)
+        curses.wrapper(draw)
         curses.curs_set(False)
