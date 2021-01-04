ce471242965f395c827e276c971468ccd1af6ad0
herypank <60302361+herypank@users.noreply.github.com>
2021-01-04 21:17:44 +0300

    Удалил лишний код

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def blink(canvas, row, column, symbol='*'):
         await asyncio.sleep(0)
 
 
-def draw(canvas):
+def draw_stars(canvas):
     courutines = []
     for courutine_row in range(1, 19):
         courutines.append(blink(canvas, row=courutine_row, column=20))
@@ -26,16 +26,13 @@ def draw(canvas):
         for courutine in courutines:
             courutine.send(None)
             draw_border(canvas)
+            canvas.border()
+            canvas.refresh()
             time.sleep(0.01)
 
 
-def draw_border(canvas):
-    canvas.border()
-    canvas.refresh()
-
-
 if __name__ == '__main__':
+    curses.initscr()
     curses.update_lines_cols()
-    curses.wrapper(draw_border)
     curses.curs_set(False)
-    curses.wrapper(draw)
+    curses.wrapper(draw_stars)
