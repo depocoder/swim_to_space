bb2ce7547a11db3117b561bae0777daf83b83b61
herypank <60302361+herypank@users.noreply.github.com>
2021-01-19 00:55:32 +0300

    add fire attack

--- main.py
+++ main.py
@@ -21,6 +21,36 @@ async def blink(canvas, row, column, symbol='*'):
         await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw_stars(canvas):
     courutines = []
     max_row, max_column = canvas.getmaxyx()
@@ -34,11 +64,15 @@ def draw_stars(canvas):
         courutine = blink(canvas, row=row, column=column, symbol=symbol)
         courutines.append(courutine)
         positon_stars.append([row, column])
+        fire_cour = fire(canvas, 10, 6)
+
     while True:
         courutine = random.choice(courutines)
         courutine.send(None)
-        canvas.border()
+        fire_cour.send(None)
         canvas.refresh()
+        canvas.border()
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
