aba6dc5d1f4f4b57cfd33079cf9aff122e465c48
Иван Попов <i.popov@MacBook-Pro-Ivan.local>
2021-08-02 22:24:24 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,79 +1,10 @@
 import os
 import time
-import asyncio
 import curses
 import random
 from itertools import cycle
 
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
-
-
-def read_controls(canvas):
-    """Read keys pressed and returns tuple witl controls state."""
-
-    rows_direction = columns_direction = 0
-    space_pressed = False
-
-    while True:
-        pressed_key_code = canvas.getch()
-
-        if pressed_key_code == -1:
-            # https://docs.python.org/3/library/curses.html#curses.window.getch
-            break
-
-        if pressed_key_code == UP_KEY_CODE:
-            rows_direction = -1
-
-        if pressed_key_code == DOWN_KEY_CODE:
-            rows_direction = 1
-
-        if pressed_key_code == RIGHT_KEY_CODE:
-            columns_direction = 1
-
-        if pressed_key_code == LEFT_KEY_CODE:
-            columns_direction = -1
-
-        if pressed_key_code == SPACE_KEY_CODE:
-            space_pressed = True
-
-    return rows_direction, columns_direction, space_pressed
-
-
-async def draw_frame(canvas, start_row, start_column, text, negative=False):
-    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
-    while True:
-        rows_number, columns_number = canvas.getmaxyx()
-
-        for row, line in enumerate(text.splitlines(), round(start_row)):
-            if row < 0:
-                continue
-
-            if row >= rows_number:
-                break
-
-            for column, symbol in enumerate(line, round(start_column)):
-                if column < 0:
-                    continue
-
-                if column >= columns_number:
-                    break
-
-                if symbol == ' ':
-                    continue
-
-                # Check that current position it is not in a lower right corner of the window
-                # Curses will raise exception in that case. Don`t ask why…
-                # https://docs.python.org/3/library/curses.html#curses.window.addch
-                if row == rows_number - 1 and column == columns_number - 1:
-                    continue
-
-                symbol = symbol if not negative else ' '
-                canvas.addch(row, column, symbol)
-        await asyncio.sleep(0)
+from utils import blink, read_controls, draw_frame
 
 
 def get_ship_frames():
@@ -84,29 +15,6 @@ def get_ship_frames():
         with open(frame_dir, "r") as my_file:
             frames.append(my_file.read())
     return cycle(frames)
-
-
-async def sleep(times: int):
-    for _ in range(times):
-        await asyncio.sleep(0)
-
-
-async def blink(canvas, row, column, symbol='*'):
-    canvas.addstr(row, column, symbol, curses.A_DIM)
-    await sleep(random.randint(1, 100))
-
-    while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await sleep(20)
-
-        canvas.addstr(row, column, symbol)
-        await sleep(3)
-
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await sleep(5)
-
-        canvas.addstr(row, column, symbol)
-        await sleep(3)
 
 
 def generate_stars_coroutines(canvas, count: int = 100) -> list:
@@ -126,36 +34,6 @@ def generate_stars_coroutines(canvas, count: int = 100) -> list:
     return coroutines
 
 
-async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
-    """Display animation of gun shot, direction and speed can be specified."""
-
-    row, column = start_row, start_column
-
-    canvas.addstr(round(row), round(column), '*')
-    await asyncio.sleep(0)
-
-    canvas.addstr(round(row), round(column), 'O')
-    await asyncio.sleep(0)
-    canvas.addstr(round(row), round(column), ' ')
-
-    row += rows_speed
-    column += columns_speed
-
-    symbol = '-' if columns_speed else '|'
-
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 1, columns - 1
-
-    curses.beep()
-
-    while 0 < row < max_row and 0 < column < max_column:
-        canvas.addstr(round(row), round(column), symbol)
-        await asyncio.sleep(0)
-        canvas.addstr(round(row), round(column), ' ')
-        row += rows_speed
-        column += columns_speed
-
-
 def change_control(
         max_row, max_column, ship_row, ship_column, rows_direction, columns_direction
 ):
@@ -173,12 +51,9 @@ def change_control(
 def draw(canvas):
     canvas.nodelay(True)
     coroutines = generate_stars_coroutines(canvas)
-    fire_coroutines = []
     max_row, max_column = canvas.getmaxyx()
 
     ship_row, ship_column = max_row // 2, max_column // 2
-    fire_coroutines.append(fire(canvas, 10, 6))
-    fire_coroutines.append(fire(canvas, 5, 3))
     ship_frames = get_ship_frames()
     ship_frame = next(ship_frames)
     while True:
---
+++ utils.py
@@ -0,0 +1,126 @@
+import asyncio
+import curses
+import random
+
+
+SPACE_KEY_CODE = 32
+LEFT_KEY_CODE = 260
+RIGHT_KEY_CODE = 261
+UP_KEY_CODE = 259
+DOWN_KEY_CODE = 258
+
+
+async def sleep(times: int):
+    for _ in range(times):
+        await asyncio.sleep(0)
+
+
+async def blink(canvas, row, column, symbol='*'):
+    canvas.addstr(row, column, symbol, curses.A_DIM)
+    await sleep(random.randint(1, 100))
+
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await sleep(20)
+
+        canvas.addstr(row, column, symbol)
+        await sleep(3)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await sleep(5)
+
+        canvas.addstr(row, column, symbol)
+        await sleep(3)
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+def read_controls(canvas):
+    """Read keys pressed and returns tuple witl controls state."""
+
+    rows_direction = columns_direction = 0
+    space_pressed = False
+
+    while True:
+        pressed_key_code = canvas.getch()
+
+        if pressed_key_code == -1:
+            # https://docs.python.org/3/library/curses.html#curses.window.getch
+            break
+
+        if pressed_key_code == UP_KEY_CODE:
+            rows_direction = -1
+
+        if pressed_key_code == DOWN_KEY_CODE:
+            rows_direction = 1
+
+        if pressed_key_code == RIGHT_KEY_CODE:
+            columns_direction = 1
+
+        if pressed_key_code == LEFT_KEY_CODE:
+            columns_direction = -1
+
+        if pressed_key_code == SPACE_KEY_CODE:
+            space_pressed = True
+
+    return rows_direction, columns_direction, space_pressed
+
+
+async def draw_frame(canvas, start_row, start_column, text, negative=False):
+    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
+    while True:
+        rows_number, columns_number = canvas.getmaxyx()
+
+        for row, line in enumerate(text.splitlines(), round(start_row)):
+            if row < 0:
+                continue
+
+            if row >= rows_number:
+                break
+
+            for column, symbol in enumerate(line, round(start_column)):
+                if column < 0:
+                    continue
+
+                if column >= columns_number:
+                    break
+
+                if symbol == ' ':
+                    continue
+
+                # Check that current position it is not in a lower right corner of the window
+                # Curses will raise exception in that case. Don`t ask why…
+                # https://docs.python.org/3/library/curses.html#curses.window.addch
+                if row == rows_number - 1 and column == columns_number - 1:
+                    continue
+
+                symbol = symbol if not negative else ' '
+                canvas.addch(row, column, symbol)
+        await asyncio.sleep(0)
