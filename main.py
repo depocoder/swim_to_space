7015630963674bba781bc600058f522ce8386e19
i.popov <i.popov@okenit.ru>
2021-11-14 14:48:54 +0300

    add garbage

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ import random
 from itertools import cycle
 
 from settings import BORDER
-from utils import blink, draw_ship
+from utils import blink, draw_ship, fly_garbage, sleep
+
+COROUTINES = []
 
 
-def get_ship_frames():
+def get_cycle_frames(dir_to_frames, need_double=True):
     frames = []
-    frame_dirs = [os.path.join('rocket_frames', frame) for frame in os.listdir('rocket_frames')]
+    frame_dirs = [os.path.join(f'frames/{dir_to_frames}', frame) for frame in os.listdir(f'frames/{dir_to_frames}')]
     for frame_dir in frame_dirs:
         with open(frame_dir, "r") as my_file:
             frame = my_file.read()
             frames.append(frame)
-            frames.append(frame)
+            if need_double:
+                frames.append(frame)
     return cycle(frames)
 
 
-def generate_stars_coroutines(canvas, count: int = 1000) -> list:
+def generate_stars_coroutines(canvas, count: int = 200) -> list:
     coroutines = []
     max_row, max_column = canvas.getmaxyx()
     stars_position = set()
@@ -36,17 +39,30 @@ def generate_stars_coroutines(canvas, count: int = 1000) -> list:
     return coroutines
 
 
+async def fill_orbit_with_garbage(canvas):
+    max_row, max_column = canvas.getmaxyx()
+    garbage_frames = get_cycle_frames('garbage_frames', need_double=False)
+    while True:
+        await sleep(15)
+        global COROUTINES
+        garbage_column = random.randint(1, max_column + BORDER)
+        COROUTINES.append(
+            fly_garbage(canvas, column=garbage_column, garbage_frame=next(garbage_frames))
+        )
+
+
 def draw(canvas):
     canvas.nodelay(True)
-    coroutines = generate_stars_coroutines(canvas)
-    ship_frames = get_ship_frames()
-    coroutines.append(draw_ship(canvas, ship_frames))
+    COROUTINES.append(fill_orbit_with_garbage(canvas))
+    COROUTINES.extend(generate_stars_coroutines(canvas))
+    ship_frames = get_cycle_frames('rocket_frames')
+    COROUTINES.append(draw_ship(canvas, ship_frames))
     while True:
-        for coroutine in coroutines:
+        for coroutine in COROUTINES:
             try:
                 coroutine.send(None)
             except StopIteration:
-                coroutines.remove(coroutine)
+                COROUTINES.remove(coroutine)
         canvas.border()
         canvas.refresh()
         time.sleep(0.1)
--- utils.py
+++ utils.py
@@ -152,3 +152,19 @@ async def draw_ship(canvas, ship_frames):
             ship_column, rows_direction, columns_direction
         )
 
+
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    row = 0
+
+    while row < rows_number:
+        draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
+
